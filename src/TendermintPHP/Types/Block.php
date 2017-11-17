738d8e33956e07c5801a71c698c0b8afbd0786e9
Cesar Rodriguez <kesarr@gmail.com>
2017-11-17 19:50:29 +0100

    block wip

--- src/TendermintPHP/Types/Block.php
+++ src/TendermintPHP/Types/Block.php
@@ -4,8 +4,17 @@ namespace TendermintPHP\Types;
 
 class Block
 {
+    private $height;
+    private $hash;
+    private $chainId;
+    private $time;
+    private $validatorsHash;
+    private $appHash;
+    private $transactions;
+
     public function __construct(array $response)
     {
+        $this->height = $response['block']['header']['height'];
         $this->hash = new BlockHash($response['block_meta']['block_id']['hash']);
         $this->chainId = $response['block']['header']['chain_id'];
         $this->time = $response['block']['header']['time'];
@@ -15,5 +24,40 @@ class Block
         foreach ($response['block']['data']['txs'] as $transaction) {
             $this->transactions[] = new TransactionHash($transaction);
         }
+    }
+
+    public function height(): int
+    {
+        return $this->height;
+    }
+
+    public function hash(): BlockHash
+    {
+        return $this->hash;
+    }
+
+    public function chainId(): string
+    {
+        return $this->chainId;
+    }
+
+    public function time(): string // TODO: make DateTime
+    {
+        return $this->time;
+    }
+
+    public function validatorsHash(): Hash
+    {
+        return $this->validatorsHash;
+    }
+
+    public function appHash(): ?string
+    {
+        return $this->appHash;
+    }
+
+    public function transactions(): array
+    {
+        return $this->transactions;
     }
 }
