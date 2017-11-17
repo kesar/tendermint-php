bdd82411d3f0ee4488be882934277511bd560874
Cesar Rodriguez <kesarr@gmail.com>
2017-11-17 20:20:29 +0100

    transactions in tendermint

--- src/TendermintPHP/Types/TransactionHash.php
+++ src/TendermintPHP/Types/TransactionHash.php
@@ -2,6 +2,25 @@
 
 namespace TendermintPHP\Types;
 
-class TransactionHash extends Hash
+class TransactionHash
 {
+    protected $hash;
+
+    public function __construct(string $hash)
+    {
+        if (strlen($hash) !== 140) {
+            throw new \LengthException($hash.' is not valid.');
+        }
+        $this->hash = $hash;
+    }
+
+    public function __toString()
+    {
+        return $this->hash;
+    }
+
+    public function toString()
+    {
+        return $this->hash;
+    }
 }
