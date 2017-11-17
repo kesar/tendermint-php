c0f8be62522bf1cf4048b6b55a74a66563ed3cc1
Cesar Rodriguez <kesarr@gmail.com>
2017-11-17 20:03:59 +0100

    update

--- example.php
+++ example.php
@@ -6,6 +6,6 @@ include 'vendor/autoload.php';
 
 $client = new TendermintClient('http://localhost:46657');
 
-//print_r($client->info()->abciInfo());
+print_r($client->info()->abciInfo());
 print_r($client->blocks()->block(1));
 echo $client->blocks()->blockNumber();
